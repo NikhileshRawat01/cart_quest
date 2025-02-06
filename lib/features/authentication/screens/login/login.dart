a7eb4331371b85349309c8b4c22ef01c0a09d53f
architbinji <Abinji9601@conestogac.on.ca>
2025-02-06 10:27:29 -0500

    splash.yaml pushed

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -4,6 +4,7 @@ import '../../../../common/styles/spacing_styles.dart';
 import '../../../../helpers/helper_functions.dart';
 import '../../../../utils/constants/image_strings.dart';
 import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/constants/text_strings.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -29,7 +30,37 @@ class LoginScreen extends StatelessWidget {
                   const SizedBox(height: TSizes.sm),
                   Text(TTexts.loginSubTitle, style: Theme.of(context).textTheme.bodyMedium),
                 ],
-              )
+              ),
+
+              /// form
+              Form(child: Column(
+                children:[
+                  /// email
+                  TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Iconsax.direct_right), labelText: TTexts.email,),
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwInputFields,),
+
+                  /// password
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      prefixIcon: Icon(Iconsax.password_check),
+                      labelText: TTexts.password,
+                      suffixIcon: Icon(Iconsax.eye_slash),
+                    ),
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwInputFields / 2,),
+
+                  /// remember me & forget password
+                  Row(
+                    children: [
+                      Checkbox(value: true, onChanged: (value){}),
+          const Text(TTexts.rememberMe),
+                    ],
+                  )
+                ],
+              ),)
             ],
           ),
         ),
---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,60 @@
+
+/// this class contains all the app text in string format
+class TTexts{
+
+  // -- global texts
+  static const String submit = "Submit";
+  static const String appName = "Cart Quest";
+  static const String tContinue = "Continue";
+
+  // -- onboarding texts
+  static const String onBoardingTitle1 = "Choose your product";
+  static const String onBoardingTitle2 = "Select Payment Method";
+  static const String onBoardingTitle3 = "Deliver at your door step";
+
+  static const String onBoardingSubTitle1 = "Welcome to a world of limitless choices";
+  static const String onBoardingSubTitle2 = "For seamless transactions, choose your payment method";
+  static const String onBoardingSubTitle3 = "From our doorstep to yours";
+
+  // --home
+  static const String homeAppbarTitle = "Good day for shopping";
+  static const String homeAppbarSubTitle = "Cart Quest";
+
+  //-- Authentication form text
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+  static const String email = "E-Mail";
+  static const String password = "Password";
+  static const String newPassword = "New Password";
+  static const String username = "Username";
+  static const String phoneNo = "Phone Number";
+  static const String rememberMe = "Remember Me";
+  static const String forgetPassword = "Forget Password?";
+  static const String signIn = "Sign In";
+  static const String createAccount = "Create Account";
+  static const String orSignInWith = "or sign in with";
+  static const String orSignUpWith = "or sign up with";
+  static const String iAgreeTo = "I agree to";
+  static const String privacyPolicy = "Privacy Policy";
+  static const String termsOfUse = "Terms of use";
+  static const String verificationCode = "verification Code";
+  static const String resendEmail = "Resend Email";
+  static const String resendEmailIn = "Resend Email in";
+
+  //-- authentication headings text
+  static const String loginTitle = "Welcome back";
+  static const String loginSubTitle = "Discover limitless choices and unmatched convenience";
+  static const String signupTitle = "let's create your account";
+  static const String forgetPasswordTitle = "Forget Password";
+  static const String forgetPasswordSubTitle = "Don't worry sometimes people can forget too, enter your email and we will send you a password reset link";
+  static const String changeYourPasswordTitle = "Password Reset Email Sent";
+  static const String changeYourPasswordSubTitle = "Your account Security is our Priority, We've sent you a secure link to safely change your password and keep your account protected";
+  static const String confirmEmail = "verify your email address";
+  static const String confirmEmailSubTitle = "Congratulations! your account awaits: verify your email to start shopping and experience a world of best deals";
+  static const String emailNotReceivedMessage = "Didn't get the email? check your junk/spam or resend it.";
+  static const String yourAccountCreatedTitle = "Your account successfully created";
+  static const String yourAccountCreatedSubTitle = "Welcome to your ultimate destination: your account is created, unleash the joy of seamless online shopping";
+
+
+
+}
